5a389f7770faf91c43250fda52eb217dc541f78f
Manraj Singh <manraj.saini123@gmail.com>
2023-12-31 15:12:59 +0530

    user controller, to add user, done

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,4 +1,27 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+import * as bcrypt from 'bcrypt';
 
 @Controller('users')
-export class UsersController {}
+export class UsersController {
+    constructor(private readonly usersService: UsersService){}
+
+    @Post('/signup')
+    async addUser(
+        @Body('password') userPassword: string,
+        @Body('username') userName: string,
+    ){
+        const salt = 10;
+        const hashedPassword = await bcrypt.hash(userPassword, salt);
+        const result = await this.usersService.insertUser(
+            userName,
+            hashedPassword,
+        );
+
+        return {
+            msg: 'User successfully registered',
+            userId: result.id,
+            userName: result.username,
+        };
+    }
+}
